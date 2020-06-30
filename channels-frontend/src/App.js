f2741a73eec7ae8b9828a72926be21fbc94dc599
dbsasser <danielbsasser@gmail.com>
2020-06-30 12:43:56 -0400

    first commit

---
+++ channels-frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component() {
+  
+  render() {
+    return (
+      <div className="App">
+        App
+      </div>
+    );
+  }
+
+}
+
+export default App;
