ddd776f8f898c4a73da65370eab95f821c1c8a78
dbsasser <danielbsasser@gmail.com>
2020-07-01 16:28:47 -0400

    Set up controller actions

--- channels-backend/app/controllers/api/v1/channels_controller.rb
+++ channels-backend/app/controllers/api/v1/channels_controller.rb
@@ -1,2 +1,34 @@
 class Api::V1::ChannelsController < ActionController::API
+
+    def index 
+        @channels = Channel.all
+        render json: @channels
+    end
+
+    def create
+        @channel = Channel.new(channel_params)
+        @channel.owner_id = 1 #Temporary set owner until current_user set up
+        if @channel.save
+            render json: @channel
+        else 
+            render json: {error: 'Trouble creating channel'}
+        end 
+    end
+
+    def show
+        @channel = Channel.find(params[:id])
+        render json: @channel
+    end 
+
+    def destroy
+        @channel = Channel.find(params[:id])
+        @channel.destroy
+    end
+
+    private
+
+    def channel_params 
+        params.require(:channel).permit(:name, :description)
+    end
+
 end
