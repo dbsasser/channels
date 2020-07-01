7538c718dcc707c259e0883533104f8d92fc00c1
dbsasser <danielbsasser@gmail.com>
2020-07-01 09:37:43 -0400

    Added has secure password

--- channels-backend/app/models/user.rb
+++ channels-backend/app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     has_many :messages
     has_many :channels
     has_many :owned_channels, class_name: 'Channel', foreign_key: "owner_id"
+    has_secure_password
 end
