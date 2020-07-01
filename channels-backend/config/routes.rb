b93810c3394755a14c39da2d3ada3494edd41beb
dbsasser <danielbsasser@gmail.com>
2020-07-01 15:06:34 -0400

    namespaced routes to api/v1

--- channels-backend/config/routes.rb
+++ channels-backend/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users
-  resources :channels
-  resources :messages
-  
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      resources :channels
+      resources :messages
+    end
+  end
+
 end
