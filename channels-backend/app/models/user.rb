347d3b512d4e8fc31288ca83f2bf92ccb6415e37
dbsasser <danielbsasser@gmail.com>
2020-07-01 16:05:43 -0400

    Fixed typo in validations

--- channels-backend/app/models/user.rb
+++ channels-backend/app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
     has_many :channels, through: :memberships
     has_many :owned_channels, class_name: 'Channel', foreign_key: "owner_id"
     validates :name, presence: true
-    validate :email, uniqueness: true
+    validates :email, uniqueness: true
     validates :password, presence: true
     has_secure_password
     
