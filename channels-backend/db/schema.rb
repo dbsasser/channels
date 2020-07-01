375a0cfaa090505d72e2d50add6ee4b319f1b667
dbsasser <danielbsasser@gmail.com>
2020-07-01 09:32:19 -0400

    Created Message model

--- channels-backend/db/schema.rb
+++ channels-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_01_132657) do
+ActiveRecord::Schema.define(version: 2020_07_01_133104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 2020_07_01_132657) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.bigint "channel_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["channel_id"], name: "index_messages_on_channel_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -31,4 +40,6 @@ ActiveRecord::Schema.define(version: 2020_07_01_132657) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "messages", "channels"
+  add_foreign_key "messages", "users"
 end
