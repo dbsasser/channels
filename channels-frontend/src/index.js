b7fb41e876bec358a1e36e009c342a22083bd3c1
dbsasser <danielbsasser@gmail.com>
2020-07-02 12:49:17 -0400

    Imported packages needed for Redux

--- channels-frontend/src/index.js
+++ channels-frontend/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk'
+impore {Provider} from 'react-redux'
 
 ReactDOM.render(
   <React.StrictMode>
