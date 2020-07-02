8f2ad5e6a8470cb4b9bff0513eff3b464eff1bf4
dbsasser <danielbsasser@gmail.com>
2020-07-01 20:14:38 -0400

    Set up controller actions

--- channels-backend/app/controllers/api/v1/messages_controller.rb
+++ channels-backend/app/controllers/api/v1/messages_controller.rb
@@ -1,2 +1,39 @@
 class Api::V1::MessagesController < ActionController::API
+
+    before_action :set_channel
+
+    def index 
+        render json: @channel.messages
+    end 
+
+    def show 
+        @message = @channel.find(params[:id])
+        render json: @message
+    end
+
+    def create
+        @message = @channel.messages.build(message_params)
+        @message.user_id = 1 #Temporary set user until current_user set up
+        if @message.save
+            render json: @channel
+        else 
+            render json: {error: 'Trouble Posting Message' }
+        end 
+    end
+
+
+    def destroy
+        @message = Membership.find(params[:id])
+        @message.destroy
+    end
+
+    private
+
+    def set_channel
+        @channel = Account.find(params[:channel_id])
+    end
+
+    def message_params 
+        params.require(:memssage).permit(:channel_id, :user_id)
+    end
 end
