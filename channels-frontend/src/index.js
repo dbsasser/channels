b92e71da6030c83b408ff9078278f56c965bdcf8
dbsasser <danielbsasser@gmail.com>
2020-07-02 13:00:32 -0400

    Created store, tweaked package imports

--- channels-frontend/src/index.js
+++ channels-frontend/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk'
-impore {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
+
+let store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
