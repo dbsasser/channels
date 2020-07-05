33cffaebff5f5ec073c858378c6eadacca2deadc
dbsasser <danielbsasser@gmail.com>
2020-07-04 23:05:52 -0400

    Mapped messages prop to Message component

--- channels-frontend/src/containers/ChannelContainer.js
+++ channels-frontend/src/containers/ChannelContainer.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import Message from '../components/Message'
+import {fetchMessages} from '../actions/fetchMessages'
+
 
 class ChannelContainer extends Component {
+
+    componentDidMount() {
+        this.props.fetchMessages()
+    }
 
     render() {
         return (
             <div>
-                <Message />
+                {this.props.messages.map(message => <Message message={message} />)}
             </div>
         )
     }
 }
 
-export default ChannelContainer
+const mapStateToProps = state => {
+    return {
+        messages: state.messages
+    }
+
+}
+export default connect(mapStateToProps, {fetchMessages} )(ChannelContainer)
