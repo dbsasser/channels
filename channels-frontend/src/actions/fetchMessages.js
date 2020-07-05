d989cb9b18e84c671d66753b78640abaae90a31d
dbsasser <danielbsasser@gmail.com>
2020-07-04 21:59:14 -0400

    Set up fetchMessages action

--- channels-frontend/src/actions/fetchMessages.js
+++ channels-frontend/src/actions/fetchMessages.js
@@ -1,3 +1,10 @@
 export function fetchMessages() {
-    
+    return (dispatch) => {
+    fetch('http://localhost:3000/api/v1/channels/1/messages')
+      .then(resp => resp.json())
+      .then(messages => dispatch({
+          type: 'FETCH_MESSAGES',
+          payload: messages
+      }))
+  }
 }
