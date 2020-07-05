ba91ca66f3d96a210db50b25b2f1257c109de1be
dbsasser <danielbsasser@gmail.com>
2020-07-04 23:05:34 -0400

    Added date_and_time method to show date/time

--- channels-backend/app/serializers/message_serializer.rb
+++ channels-backend/app/serializers/message_serializer.rb
@@ -1,4 +1,8 @@
 class MessageSerializer < ActiveModel::Serializer
-  attributes :id, :body 
+  attributes :id, :body, :date_and_time
   belongs_to :user
+
+  def date_and_time
+    return date_and_time = object.created_at.strftime("%D at %I:%M %p")
+  end
 end
