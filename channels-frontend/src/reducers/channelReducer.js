f9ac9823f029ab57e34675c14a5f60c54708881d
dbsasser <danielbsasser@gmail.com>
2020-07-06 02:39:29 -0400

    Added fetch_user action

--- channels-frontend/src/reducers/channelReducer.js
+++ channels-frontend/src/reducers/channelReducer.js
@@ -5,10 +5,14 @@ export default function channelReducer(state = {messages: [], users_channels: []
             return {...state, messages: action.payload}
 
         case 'SEND_MESSAGE':
-            return {messages: action.payload}
+            return {...state, messages: action.payload}
 
         case 'FETCH_USERS_CHANNELS':
             return {...state, users_channels: action.payload}
+        
+        case 'FETCH_USER':
+            return {...state, user: action.payload, loggedIn: true}
+
             
         default:
             return state
