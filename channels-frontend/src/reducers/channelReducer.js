68e46797d1aaf8480d0e1306c6f671704430de0e
dbsasser <danielbsasser@gmail.com>
2020-07-05 21:31:34 -0400

    Added send_message case

--- channels-frontend/src/reducers/channelReducer.js
+++ channels-frontend/src/reducers/channelReducer.js
@@ -3,6 +3,10 @@ export default function channelReducer(state = {messages: []}, action) {
     switch (action.type) {
         case 'FETCH_MESSAGES':
             return {messages: action.payload}
+
+        case 'SEND_MESSAGE':
+            return {messages: action.payload}
+            
         default:
             return state
     }
