d7486ced14728c4fd57d193d040bbfca1c9470a1
dbsasser <danielbsasser@gmail.com>
2020-07-05 23:10:38 -0400

    imported and set up Router and Route

--- channels-frontend/src/index.js
+++ channels-frontend/src/index.js
@@ -5,6 +5,8 @@ import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import channelReducer from './reducers/channelReducer'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import loginForm from './components/loginForm'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 let store = createStore(channelReducer, composeEnhancers(applyMiddleware(thunk)))
@@ -12,7 +14,10 @@ let store = createStore(channelReducer, composeEnhancers(applyMiddleware(thunk))
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
+      <Router>
+        <Route exact path="/login" component={loginForm} />
+        <Route path="/channels/:id" component={App} />
+      </Router>
     </React.StrictMode>
   </Provider>,
   document.getElementById('root')
