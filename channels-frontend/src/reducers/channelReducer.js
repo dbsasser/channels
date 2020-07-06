0e8c90ccaed353933bf34efe7c090589b9c8f355
dbsasser <danielbsasser@gmail.com>
2020-07-05 22:08:54 -0400

    Added FETCH_USERS_CHANNELS

--- channels-frontend/src/reducers/channelReducer.js
+++ channels-frontend/src/reducers/channelReducer.js
@@ -1,11 +1,14 @@
-export default function channelReducer(state = {messages: []}, action) {
+export default function channelReducer(state = {messages: [], users_channels: []}, action) {
 
     switch (action.type) {
         case 'FETCH_MESSAGES':
-            return {messages: action.payload}
+            return {...state, messages: action.payload}
 
         case 'SEND_MESSAGE':
             return {messages: action.payload}
+
+        case 'FETCH_USERS_CHANNELS':
+            return {...state, users_channels: action.payload}
             
         default:
             return state
