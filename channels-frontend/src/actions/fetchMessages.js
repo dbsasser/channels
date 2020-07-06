dc085358b0f872e3bd11df4ebba76be8c2b2e29d
dbsasser <danielbsasser@gmail.com>
2020-07-06 11:51:40 -0400

    Use argument in fetch link instead of hardcoded

--- channels-frontend/src/actions/fetchMessages.js
+++ channels-frontend/src/actions/fetchMessages.js
@@ -1,6 +1,6 @@
-export function fetchMessages() {
+export function fetchMessages(channel_id) {
     return (dispatch) => {
-    fetch('http://localhost:3000/api/v1/channels/1/messages')
+    fetch(`http://localhost:3000/api/v1/channels/${channel_id}/messages`)
       .then(resp => resp.json())
       .then(messages => dispatch({
           type: 'FETCH_MESSAGES',
