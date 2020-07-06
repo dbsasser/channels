9ebd948c5fe7f7c3aa5b9ff9407f5710a5917556
dbsasser <danielbsasser@gmail.com>
2020-07-06 11:51:19 -0400

    Changed componenet in /channels/:id path

--- channels-frontend/src/index.js
+++ channels-frontend/src/index.js
@@ -7,6 +7,7 @@ import { Provider } from 'react-redux'
 import channelReducer from './reducers/channelReducer'
 import {BrowserRouter as Router, Route, Switch, } from 'react-router-dom'
 import loginForm from './components/loginForm'
+import ChannelContainer from './containers/ChannelContainer';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 let store = createStore(channelReducer, composeEnhancers(applyMiddleware(thunk)))
@@ -17,7 +18,7 @@ ReactDOM.render(
       <Router>
         <Switch>
           <Route exact path="/login" component={loginForm} />
-          <Route path="/channels/:id" component={App} />
+          <Route path="/channels/:id" component={ChannelContainer} />
         </Switch>
       </Router>
     </React.StrictMode>
