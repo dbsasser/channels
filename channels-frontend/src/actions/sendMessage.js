78bd480c5a2fbce62d3b95b38279c3e73a0a49da
dbsasser <danielbsasser@gmail.com>
2020-07-05 21:33:13 -0400

    Added sendMessage action

---
+++ channels-frontend/src/actions/sendMessage.js
@@ -0,0 +1,24 @@
+export function sendMessage(message) {
+    return (dispatch) => {
+        fetch('http://localhost:3000/api/v1/channels/1/messages', {
+            method: 'POST', // or 'PUT'
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(message),
+        })
+    // fetch('http://localhost:3000/api/v1/channels/1/messages', {
+    //     // header: {
+    //     //     'Content-Type': 'application/json',
+    //     //     'Accept':'application/json'
+    //     // },
+    //     method: 'POST',
+    //     body: JSON.stringify(message)
+    // })
+    //   .then(resp => resp.json())
+    //   .then(messages => dispatch({
+    //       type: 'FETCH_MESSAGES',
+    //       payload: messages
+    //   }))
+  }
+}
