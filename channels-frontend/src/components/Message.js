0b469abbbecf598008e17cde3add84dc5186e289
dbsasser <danielbsasser@gmail.com>
2020-07-08 18:50:37 -0400

    Heavy restyling of render

--- channels-frontend/src/components/Message.js
+++ channels-frontend/src/components/Message.js
@@ -3,10 +3,19 @@ import React from 'react'
 const Message = ({message}) => {
 
     return (
-        <div key={message.id}>
-            {message.user.name} {message.date_and_time} <br></br>
-            {message.body}
+        <>
+        <div key={message.id} className="container">
+            <div className="row">
+                <div className="col-md-auto">
+                    <img src="/default-avatar.jpg" className="rounded" width="38" height="38"></img>
+                </div>
+                <div className="col">
+                <strong>{message.user.name}</strong>   <small>{message.date_and_time}</small><br />
+                {message.body}
+                </div>
+            </div>
         </div>
+        </>           
     )
 }
 
