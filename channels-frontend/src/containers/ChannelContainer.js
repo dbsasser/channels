3d1b322bbaa7aac283c8ace0fca954361128ba69
dbsasser <danielbsasser@gmail.com>
2020-07-08 14:04:16 -0400

    Removed componentDidMount and reworked render

--- channels-frontend/src/containers/ChannelContainer.js
+++ channels-frontend/src/containers/ChannelContainer.js
@@ -1,42 +1,34 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
 import Message from '../components/Message'
 import MessageInput from '../components/MessageInput'
 import SidebarContainer from '../containers/SidebarContainer'
 import {fetchMessages} from '../actions/fetchMessages'
+import ActionCable from 'actioncable'
+import ChatWindow from './ChatWindow'
 
 
 class ChannelContainer extends Component {
 
-    componentDidMount() {
-        this.props.fetchMessages(this.props.match.params.id)
-    }
-
-
-
     render() {
         return (
             <>
-            <div className="sidenav">
-                <SidebarContainer />
+            <div className="chat-window">
+                <ChatWindow key={this.props.match.params.id} channel_id={this.props.match.params.id} />
             </div>
-            <div className="main">
-                <div>
-                    {this.props.messages.map(message => <Message message={message} />)}
-                </div>
-                <div>
-                    <MessageInput />
-                </div>
+            <div className="message-input">
+                <MessageInput key={this.props.match.params.id} channel_id={this.props.match.params.id} />
             </div>
             </>
         )
     }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
     return {
-        messages: state.messages
+        channel_id: state.channel_id
     }
-
 }
-export default connect(mapStateToProps, {fetchMessages} )(ChannelContainer)
+
+export default withRouter(connect(mapStateToProps)(ChannelContainer))
