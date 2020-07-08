24705da520b5c207ed1565bb2262bb2f9a2a79b6
dbsasser <danielbsasser@gmail.com>
2020-07-08 18:17:28 -0400

    Added signout link and event function

--- channels-frontend/src/containers/SidebarContainer.js
+++ channels-frontend/src/containers/SidebarContainer.js
@@ -11,10 +11,19 @@ class SidebarContainer extends Component {
         this.props.getCurrentUser()
     }
 
+    handleSignOut = event => {
+        event.preventDefault()
+        localStorage.removeItem('token')
+        console.log('signed out')
+        window.location.replace("http://localhost:3001/login");
+
+    }
+
     render() {
         return (
             <>
             <User user={this.props.user}/>
+            <a onClick={this.handleSignOut}>Sign Out</a>
             <UsersChannelsList channels={this.props.users_channels}/>
             </>
         )
