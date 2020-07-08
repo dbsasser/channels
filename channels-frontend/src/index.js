28a4bda1ca7d50f1cc4ac2ac21a4789773775bf3
dbsasser <danielbsasser@gmail.com>
2020-07-08 15:37:21 -0400

    Changed /channels/:id to display App component

--- channels-frontend/src/index.js
+++ channels-frontend/src/index.js
@@ -7,7 +7,6 @@ import { Provider } from 'react-redux'
 import channelReducer from './reducers/channelReducer'
 import {BrowserRouter as Router, Route, Switch, } from 'react-router-dom'
 import loginForm from './components/loginForm'
-import ChannelContainer from './containers/ChannelContainer';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 let store = createStore(channelReducer, composeEnhancers(applyMiddleware(thunk)))
@@ -15,13 +14,13 @@ let store = createStore(channelReducer, composeEnhancers(applyMiddleware(thunk))
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
-      <Router>
-        <Switch>
-          <Route exact path="/login" component={loginForm} />
-          <Route path="/channels/:id" component={ChannelContainer} />
-        </Switch>
-      </Router>
-    </React.StrictMode>
+        <Router>
+          <Switch>
+            <Route exact path="/login" component={loginForm} />
+            <Route exact path="/channels/:id" component={App}  />
+          </Switch>
+        </Router>
+      </React.StrictMode>
   </Provider>,
   document.getElementById('root')
 );
