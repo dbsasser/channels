2549d1d440d08ddbec2ab4a6859f70379ead98c4
dbsasser <danielbsasser@gmail.com>
2020-07-08 18:15:37 -0400

    Import and adde /signup route

--- channels-frontend/src/index.js
+++ channels-frontend/src/index.js
@@ -7,6 +7,7 @@ import { Provider } from 'react-redux'
 import channelReducer from './reducers/channelReducer'
 import {BrowserRouter as Router, Route, Switch, } from 'react-router-dom'
 import loginForm from './components/loginForm'
+import signupForm from './components/signupForm'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 let store = createStore(channelReducer, composeEnhancers(applyMiddleware(thunk)))
@@ -17,6 +18,7 @@ ReactDOM.render(
         <Router>
           <Switch>
             <Route exact path="/login" component={loginForm} />
+            <Route exact path="/signup" component={signupForm} />
             <Route exact path="/channels/:id" component={App}  />
           </Switch>
         </Router>
