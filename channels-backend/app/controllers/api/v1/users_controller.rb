cd8d453b53df5e63b56b61f9244d4f4eec1deee8
dbsasser <danielbsasser@gmail.com>
2020-07-08 18:15:18 -0400

    Built #create

--- channels-backend/app/controllers/api/v1/users_controller.rb
+++ channels-backend/app/controllers/api/v1/users_controller.rb
@@ -1,3 +1,22 @@
-class Api::V1::UsersController < ActionController::API
+class Api::V1::UsersController < ApplicationController
+
+    skip_before_action :authorized
+
+    def create
+     @user = User.new(user_params)
+     if @user.save
+        Membership.create(channel_id: 1, user_id: @user.id)
+        token = encode_token({ user_id: @user.id })
+        render json: { user: @user, channels: @user.channels, jwt: token }, status: :accepted
+    else
+        render json: { message: 'Invalid email or password' }, status: :unauthorized
+        end
+    end
+
+    private
+
+    def user_params 
+        params.require(:user).permit(:name, :email, :password)
+    end
 end
 
