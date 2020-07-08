2f8d463e3d1519f9faa891cca00290ddf8e1740c
dbsasser <danielbsasser@gmail.com>
2020-07-08 13:52:41 -0400

    Removed recieved method and contents

--- channels-backend/app/channels/chat_channel.rb
+++ channels-backend/app/channels/chat_channel.rb
@@ -2,12 +2,9 @@ class ChatChannel < ApplicationCable::Channel
   def subscribed
     @channel = Channel.find_by(id: params[:channel_id])
     stream_for @channel
-    # stream_from "channel_channel_#{params[:channel_id]}"
-    # stream_from "some_channel"
   end
 
   def received(data)
-    ChatChannel.broadcast_to(@channel, {channel: @channel, messages: @channel.messages})
   end
   
   def unsubscribed
