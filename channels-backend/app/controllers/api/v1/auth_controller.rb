1575256716be90c043ae7c101fbe3ec7097180b2
dbsasser <danielbsasser@gmail.com>
2020-07-08 18:15:05 -0400

    Added channels to json render in #create

--- channels-backend/app/controllers/api/v1/auth_controller.rb
+++ channels-backend/app/controllers/api/v1/auth_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::AuthController < ApplicationController
         @user = User.find_by(email: user_login_params[:email])
         if @user && @user.authenticate(user_login_params[:password])
             token = encode_token({ user_id: @user.id })
-            render json: { user: @user, jwt: token }, status: :accepted
+            render json: { user: @user, channels: @user.channels, jwt: token }, status: :accepted
         else
             render json: { message: 'Invalid email or password' }, status: :unauthorized
         end
