5ec55e4af2c77442899dc4da673301ca2ab2cec2
dbsasser <danielbsasser@gmail.com>
2020-07-09 15:21:27 -0400

    Restyled render and added signout link to component

--- channels-frontend/src/components/User.js
+++ channels-frontend/src/components/User.js
@@ -1,11 +1,20 @@
 import React from 'react'
 
-const User = ({user}) => {
+const User = ({user, handleSignOut}) => {
 
     return (
         <div key={user.id}>
-            {user.name}<br></br>
-            {user.email}
+            
+            <div className='row'>
+                <div className='col-md-auto ml-2 mt-1'>
+                    <img src='/default-avatar.jpg'></img>
+                </div>
+                <div className='col ml-0 pl-0'>
+                    <strong>{user.name}</strong><br />
+                    {user.email}<br />
+                    <small><a onClick={handleSignOut}>Sign Out</a></small>
+                </div>
+            </div>
         </div>
     )
 }
