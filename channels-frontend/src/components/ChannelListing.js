bff8d79413430df52461c9d0df5bfeffc469c01c
dbsasser <danielbsasser@gmail.com>
2020-07-09 15:21:50 -0400

    Built component to show channel listing

---
+++ channels-frontend/src/components/ChannelListing.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+
+const ChannelListing = ({channels}) => {
+
+    
+    return (
+
+        <>
+        <div className="card w-50 mx-auto mt-5">
+        <div class="card-header">
+            All Channels
+        </div>
+        <div className="card-body">
+        {channels.map(channel => (
+                <div key={channel.id} className='my-2'>
+                <Link to={`/channels/${channel.id}`} className="card-link"># {channel.name}</Link> | <small>{channel.users.length} Members</small><br />
+                {channel.description}
+            </div>
+        ))}
+        </div>
+        </div>
+        </>
+    )
+}
+
+export default ChannelListing
