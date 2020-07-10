bad59ec530ce80a48b6a3d0f6c511d908f61aede
dbsasser <danielbsasser@gmail.com>
2020-07-10 16:55:20 -0400

    Reworked styling

--- channels-frontend/src/components/Message.js
+++ channels-frontend/src/components/Message.js
@@ -6,12 +6,12 @@ const Message = ({message}) => {
         <>
         <div key={message.id} className="container">
             <div className="row">
-                <div className="col-md-auto">
+                <div className="col-md-auto mt-1">
                     <img src="/default-avatar.jpg" className="rounded" width="38" height="38"></img>
                 </div>
-                <div className="col">
-                <strong>{message.user.name}</strong>   <small>{message.date_and_time}</small><br />
-                {message.body}
+                <div className="col pl-0">
+                <strong>{message.user.name}</strong>   <small>{message.date_and_time}</small>
+                <p className="message-body">{message.body}</p>
                 </div>
             </div>
         </div>
