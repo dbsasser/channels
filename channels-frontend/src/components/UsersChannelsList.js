2b0b420351df5e79e12b79929225d45e9c83ab9d
dbsasser <danielbsasser@gmail.com>
2020-07-09 22:10:30 -0400

    Added link to all channels

--- channels-frontend/src/components/UsersChannelsList.js
+++ channels-frontend/src/components/UsersChannelsList.js
@@ -5,11 +5,13 @@ const UsersChannelsList = ({channels}) => {
 
     return (
        <div>
+           <strong className="ml-2">Channels</strong>
            {channels.map(channel=>(
                 <div key={channel.id}>
                     <NavLink to={`${channel.id}`} activeClassName="activenav"># {channel.name}</NavLink>
                 </div>
            ))}
+           <NavLink to="/channels/all" activeClassName="activenav">+ More Channels</NavLink>
        </div>
     )
 }
