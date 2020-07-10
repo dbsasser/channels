b3d0881e0fae681770e5d774fb5ffb241b78a87a
dbsasser <danielbsasser@gmail.com>
2020-07-09 22:13:26 -0400

    Created as container for msg form and add channel

---
+++ channels-frontend/src/containers/MessageWindow.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+import MessageInput from '../components/MessageInput'
+import AddChannel from '../components/AddChannel'
+import {joinChannel} from '../actions/joinChannel'
+
+
+
+class ChannelContainer extends Component {
+
+    handleJoinChannel = (event) => {
+        event.preventDefault();
+        this.props.joinChannel({channel_id: this.props.channel_id})
+    }
+
+    
+    render() {
+        let channel_member = this.props.user.channels.some( channel => channel['id'] == this.props.channel_id )
+        return (
+            <>
+            <div className="message-input">
+                { channel_member
+                ? <MessageInput key={this.props.match.params.id} channel_id={this.props.match.params.id} />
+                : <div><AddChannel handleJoinChannel={this.handleJoinChannel} key={this.props.match.params.id} /></div>
+                }
+            </div>
+            </>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        channel_id: state.channel.id,
+        user: state.user
+    }
+}
+
+export default withRouter(connect(mapStateToProps, {joinChannel} )(ChannelContainer))
