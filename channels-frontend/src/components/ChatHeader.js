c97c8eb3ad6f63a623c9ecc91093b5a96666b2de
dbsasser <danielbsasser@gmail.com>
2020-07-10 16:54:35 -0400

    Added leave channel button, reworked styling

--- channels-frontend/src/components/ChatHeader.js
+++ channels-frontend/src/components/ChatHeader.js
@@ -1,12 +1,20 @@
 import React from 'react'
 
-const ChatHeader = ({channel}) => {
+const ChatHeader = ({channel, leaveChannel}) => {
 
     return (
         <>
-        <div key={channel.id} className="sticky-top m-10 border-bottom">
-                <h6># {channel.name}</h6>
-                {channel.users.length} Users - {channel.description}
+        <div className="sticky-top m-10 border-bottom">
+            <div className="row">
+                <div key={channel.id} className="col">
+                    <strong># {channel.name}</strong> - <small>{channel.description}</small><br />
+                    <small>{channel.users.length} Members</small>
+                </div>
+
+                <div className="col text-right">
+                    <button className="btn btn-outline-danger btn-sm mt-2" onClick={leaveChannel}>Leave Channel</button>
+                </div>
+            </div>
         </div>
         </>           
     )
