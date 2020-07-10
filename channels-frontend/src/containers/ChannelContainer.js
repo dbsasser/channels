c17f2b2fbd92ebb23d6e42649f84788b69ef5460
dbsasser <danielbsasser@gmail.com>
2020-07-09 22:15:58 -0400

    setChannel on didmount and imported MsgWindow

--- channels-frontend/src/containers/ChannelContainer.js
+++ channels-frontend/src/containers/ChannelContainer.js
@@ -3,23 +3,33 @@ import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import Message from '../components/Message'
 import MessageInput from '../components/MessageInput'
+import AddChannel from '../components/AddChannel'
+import {joinChannel} from '../actions/joinChannel'
 import SidebarContainer from '../containers/SidebarContainer'
-import {fetchMessages} from '../actions/fetchMessages'
+import {setChannel} from '../actions/setChannel'
 import ActionCable from 'actioncable'
 import ChatWindow from './ChatWindow'
+import MessageWindow from './MessageWindow'
+
 
 
 class ChannelContainer extends Component {
 
+    componentDidMount() {
+        this.props.setChannel()
+    }
+    
     render() {
+        let channel_member = this.props.user.channels.some( channel => channel['id'] == this.props.channel_id )
         return (
             <>
             <div className="chat">
                 <ChatWindow key={this.props.match.params.id} channel_id={this.props.match.params.id} />
             </div>
-            <div className="message-input">
-                <MessageInput key={this.props.match.params.id} channel_id={this.props.match.params.id} />
+            <div>
+                <MessageWindow key={window.location.pathname} />
             </div>
+
             </>
         )
     }
@@ -27,8 +37,10 @@ class ChannelContainer extends Component {
 
 const mapStateToProps = (state) => {
     return {
-        channel_id: state.channel_id
+        channel_id: state.channel_id,
+        user: state.user
     }
 }
 
-export default withRouter(connect(mapStateToProps)(ChannelContainer))
+
+export default withRouter(connect(mapStateToProps, {setChannel} )(ChannelContainer))
