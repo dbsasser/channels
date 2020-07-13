62a715054511545e655d86717801b472925440da
dbsasser <danielbsasser@gmail.com>
2020-07-13 15:52:25 -0400

    Added alt to avatar img

--- channels-frontend/src/components/Message.js
+++ channels-frontend/src/components/Message.js
@@ -7,11 +7,11 @@ const Message = ({message}) => {
         <div key={message.id} className="container">
             <div className="row">
                 <div className="col-md-auto mt-1">
-                    <img src="/default-avatar.jpg" className="rounded" width="38" height="38"></img>
+                    <img src="/default-avatar.jpg" className="rounded" width="38" height="38" alt="default user avatar"></img>
                 </div>
                 <div className="col pl-0">
-                <strong>{message.user.name}</strong>   <small>{message.date_and_time}</small>
-                <p className="message-body">{message.body}</p>
+                    <strong>{message.user.name}</strong>   <small>{message.date_and_time}</small>
+                    <p className="message-body">{message.body}</p>
                 </div>
             </div>
         </div>
