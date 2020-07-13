87f07645c68cf40189411c0eeadc10a1e7799d41
dbsasser <danielbsasser@gmail.com>
2020-07-13 15:52:37 -0400

    Changed signout link to button

--- channels-frontend/src/components/User.js
+++ channels-frontend/src/components/User.js
@@ -7,12 +7,12 @@ const User = ({user, handleSignOut}) => {
             
             <div className='row'>
                 <div className='col-md-auto ml-2 mt-1'>
-                    <img src='/default-avatar.jpg'></img>
+                    <img src='/default-avatar.jpg' alt="default user avatar"></img>
                 </div>
                 <div className='col ml-0 pl-0'>
                     <strong>{user.name}</strong><br />
                     {user.email}<br />
-                    <small><a onClick={handleSignOut}>Sign Out</a></small>
+                    <small><button onClick={handleSignOut} className="btn btn-link">Sign Out</button></small>
                 </div>
             </div>
         </div>
