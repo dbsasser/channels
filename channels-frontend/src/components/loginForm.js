d6ba3b66034fba897e7a7722cf5a29c05a7a694d
dbsasser <danielbsasser@gmail.com>
2020-07-14 11:47:34 -0400

    Fixed html errors in render

--- channels-frontend/src/components/loginForm.js
+++ channels-frontend/src/components/loginForm.js
@@ -32,20 +32,20 @@ class loginForm extends Component {
         return (
             <>
             <div className="card w-50 mx-auto mt-5 border border-info">
-                <div class="card-header">
+                <div className="card-header">
                     Log In
                 </div>
-                <div class="card-body text-center">
+                <div className="card-body text-center">
                     <form onSubmit={this.handleSubmit}>
-                    <div class="form-group">
-                        <label for="username">Email:</label><br />
+                    <div className="form-group">
+                        <label>Email:</label><br />
                         <input type="email" name="email" value={this.state.email} onChange={this.handleChange}></input>
                     </div>
-                    <div class="form-group">
-                        <label for="password">Password:</label><br />
+                    <div className="form-group">
+                        <label>Password:</label><br />
                         <input type="password" name="password" value={this.state.password} onChange={this.handleChange}></input>
                     </div>
-                    <input type="submit" class="btn btn-info"></input>
+                    <input type="submit" className="btn btn-info"></input>
                     </form>
                 </div>
             </div>
